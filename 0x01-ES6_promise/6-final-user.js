a58a117dad56896517d4c4a4daf0d299797c8c02
Selomon1 <soya.vandy1950@gmail.com>
2024-03-06 22:33:23 +0300

    ES6 Promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,11 +7,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName)
     .then((result) => ({ status: 'fulfilled', value: result }))
     .catch((error) => ({ status: 'rejected', value: error }));
+
   promises.push(userPromise);
 
   const photoPromise = uploadPhoto(fileName)
     .then((result) => ({ status: 'fulfilled', value: result }))
     .catch((error) => ({ status: 'rejected', value: error }));
+
   promises.push(photoPromise);
 
   return Promise.all(promises);
