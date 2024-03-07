b3d1d6f2f0b6fd8b81ce3dc109bc2c6106d7d767
Selomon1 <soya.vandy1950@gmail.com>
2024-03-07 22:54:58 +0300

    ES6 Classes

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -33,14 +33,14 @@ export class StudentHolberton {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'SanFrancisco');
+const class2019 = HolbertonClass(2019, 'San Francisco');
+const class2020 = HolbertonClass(2020, 'SanFrancisco');
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const student1 = StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = StudentHolberton('John', 'Doe', class2020);
+const student3 = StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = StudentHolberton('Donald', 'Bush', class2019);
+const student5 = StudentHolberton('Jason', 'Sandler', class2019);
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
 export default listOfStudents;
