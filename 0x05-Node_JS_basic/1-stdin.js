051e2c710cdd4fbb5f36ad8b1a26522972d6dfe1
Selomon1 <soya.vandy1950@gmail.com>
2024-05-03 10:02:36 +0300

    JS Node Basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,34 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?\n');
 
-rl.question('', (input) => {
-  const name = input;
-  if (name) {
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    rl.close();
-  });
+rl.on('line'), (input) => {
+	const name = input.trim();
+	if (name) {
+		console.log(`Your name is: ${name}`);
+		rl.close();
+		console.log('This important software is now closing');
+	}
+});
+
+if(!process.stdin.isTTY) {
+	let input = '';
+	process.stdin.setEncoding('utf8');
+
+	process.stdin.on('data', (chunk) => {
+		input += chunk;
+	});
+
+	process.stdin.on('end', () => {
+		const name.input.trim();
+		if (name) {
+			console.log(`Your name is: ${name}`);
+			console.log('This important software is now closing');
+		}
+	});
 }
