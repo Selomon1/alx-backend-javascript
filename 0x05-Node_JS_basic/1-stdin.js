c332c815ee54a59f4caf1e5cf216a524a361f7bd
Selomon1 <soya.vandy1950@gmail.com>
2024-05-03 09:48:03 +0300

    JS Node Basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -11,17 +11,7 @@ rl.on('line', (input) => {
   const name = input;
   if (name) {
     console.log(`Your name is: ${name}`);
-    rl.close();
-  }
-});
-
-if (!process.stdin.isTTY) {
-  rl.question('', (input) => {
-    const name = input;
-    if (name) {
-      console.log(`Your name is: ${name}`);
-    }
-    rl.close();
     console.log('This important software is now closing');
+    rl.close();
   });
 }
